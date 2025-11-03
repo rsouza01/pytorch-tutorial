e2a455366fd6bdd224ef17099934c2e4eeb1a676
Rodrigo de Souza <rsouza01@gmail.com>
2025-11-03 13:44:07 +0000

    Clea stuff

---
+++ tmp/scrap.sh
@@ -0,0 +1,14 @@
+
+# http://public.gettysburg.edu/~marschal/clea/
+
+
+wget --recursive --no-parent --accept pdf,txt,exe,zip \
+     --domains public.gettysburg.edu \
+     --wait=1 --limit-rate=100k \
+     --user-agent="Mozilla/5.0" \
+     http://public.gettysburg.edu/~marschal/clea/
+
+wget --recursive --no-parent --accept pdf,txt,exe,zip \
+     --domains public.gettysburg.edu \
+     --user-agent="Mozilla/5.0" \
+     http://public.gettysburg.edu/~marschal/clea/
